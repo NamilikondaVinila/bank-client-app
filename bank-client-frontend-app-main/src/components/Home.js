84d9598a02920517b2801ed917c687edf6e2945b
NamilikondaVinila <156566571+NamilikondaVinila@users.noreply.github.com>
2024-07-06 10:49:02 +0530

    Add files via upload

---
+++ bank-client-frontend-app-main/src/components/Home.js
@@ -0,0 +1,10 @@
+
+export const Home = () => {
+    
+    return(
+        <div>
+            <p></p>
+            <h1>This is your one stop bank app, you can deposit, withdraw amount</h1>
+        </div>
+    );
+}
